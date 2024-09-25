b718749cbb4884df4b86b39cf5954284bcf1225e
RyanByer27 <821172@mystma.org>
2024-09-25 15:18:42 -0500

    Data

---
+++ Unit 2 - Functions and Variables/data converson.py
@@ -0,0 +1,31 @@
+# Start by copying this code block into a new file called dataconversion.py in your unit 2 folder. This is an ungraded activity.
+
+# Successfully convert all of the following variables to another type and print the result
+# If the conversion prints without errors, you did the conversion correctly
+
+a = 115         #int -> string
+b = 3.14        #float -> string
+c = "68"        #string -> int
+d = "True"      #string -> boolean
+e = True        #boolean -> string
+f = False       #boolean -> string
+g = '10110111'  #byte -> int
+h = "2.54"      #string -> float
+i = 100         #int -> float
+j = 10.0        #float -> int
+k = 254         #int -> byte
+
+num_str = str(a)
+print(num_str)
+float_str = str(b)
+print(float_str)
+num = int(c)
+print(num)
+bool_val = bool(d)
+print(bool_val)
+bool_str1 = str(e)
+print(bool_str1)
+bool_str2 = str(f)
+print(bool_str2)
+byte_int = int(g)
+print(byte_int)

fc12cdaa22dbf66c8c898354a740b4e2d7cab4da
RyanByer27 <821172@mystma.org>
2024-09-26 14:49:31 -0500

    9/26

---
+++ Assignments/SimpleMadLib.py
@@ -0,0 +1,17 @@
+# Once there was a man named [name],
+# They were [adj] and [adj],
+# thay liked to play [game or sport],
+# Thay are feeling [emotion] today,
+# becaue thay broke there [body part],
+# playing [game or sport]
+
+print("Weclome to a simple mad lib")
+# all variables
+name = input("Pick a name\n>")
+adj1 = input("pick ana adjective\n>")
+adj2 = input("pick ana adjective\n>")
+Gamesport = input("pick a game or sport\n>")
+emotion = input("pick a emotion\n>")
+bodypart = input("pick a body part\n>")
+# the final mad lib
+print(" Once there was a man named " + name + " Thay were " + adj1 + " and " + adj2 + " Thay liked to play " + Gamesport + " thay are feeling " + emotion + " today because thay broke there " + bodypart + " playing " + Gamesport + "!!!" )

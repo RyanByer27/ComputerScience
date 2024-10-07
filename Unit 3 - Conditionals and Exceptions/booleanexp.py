9834ce93fd3cea0930e6c3caa8beea375442d871
RyanByer27 <821172@mystma.org>
2024-10-07 15:18:21 -0500

    10/7

---
+++ Unit 3 - Conditionals and Exceptions/booleanexp.py
@@ -0,0 +1,9 @@
+ # kinda like a mathematical formula
+ # 14 + 10 = 24    uses aritchmatic operatoes 
+ # can only evaluate to true or false
+ # 5 > 10 = flase     boolean uses comparison operators
+ # ! = "not"
+
+print(5 > 10)
+password = input("whats the password?")
+print(password == "dog")
---
+++ Unit 3 - Conditionals and Exceptions/ifstatements.py
@@ -0,0 +1,18 @@
+temp = input("whats the temp in f?\n>")
+temp = int(temp)
+
+if temp >= 80:
+    print(str(temp) + "is Hot!")
+
+else:
+    print(str(temp) + "is cool!")
+
+
+
+pasw = input("whats the Pasword?\n>")
+
+if pasw == "skibidi68.9": 
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
+

8d5bde6150fa5fb54d5b892775b3fb3b4c587923
RyanByer27 <821172@mystma.org>
2024-10-08 15:35:44 -0500

    test

---
+++ Testes/Unit2part1.py
@@ -0,0 +1,4 @@
+a = input("pick a random word\n>")
+b = input("pick a random word\n>")
+c = input("pick a random word\n>")
+print(a + b + c)
---
+++ Testes/Unit2part2.py
@@ -0,0 +1,10 @@
+def add3():
+    a = input("pick a number")
+    int(a)
+    b = input("pick a number")
+    int(b)
+    c = input("pick a number")
+    int(c)
+    print (a + b + c)
+
+add3()

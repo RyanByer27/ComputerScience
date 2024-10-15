34bc7deed05d1f47a07e24cc5167b5cf53cca92e
RyanByer27 <821172@mystma.org>
2024-10-15 15:04:47 -0500

    10/15

---
+++ Unit 3 - Conditionals and Exceptions/nestedifstatments.py
@@ -0,0 +1,31 @@
+
+
+prime = True
+cost = 20
+age = 17
+consent = False
+
+if prime:
+    if age >= 18:
+        print("FREE SHIPING APPLIED")
+    elif consent:
+        print("FREE SHIPING APPLIED")
+    else:
+        print("no free  shiping")
+
+elif cost >= 25:
+     if age >= 18:
+        print("FREE SHIPING APPLIED")
+     elif consent:
+        print("FREE SHIPING APPLIED")
+     else:
+        print("no free  shiping")
+
+else:
+    print("no free  shiping")
+
+
+
+
+raining = input("Is it raining outside?\n>")
+outside = input("Do you plan on going outside?'\n>")

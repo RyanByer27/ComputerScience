c5e5b9bc217bc887a563894eb042e657b57950f0
RyanByer27 <821172@mystma.org>
2024-10-23 15:19:59 -0500

    10/23

--- Assignments/Textproj.py
+++ Assignments/Textproj.py
@@ -1,299 +1,71 @@
 # Weclome to my game in this game there will be 5 paths with 5 difernt endings and 2 hidden endings
-# In the game you will be going throug a school day
+# In the game you will be going through a school day
 # A key to navageting the code is #1 is path 1 #2 is path 2 and so on
-def Waking_Up():
+def Waking_Up():#Start
     print("You Just woke up it is 7am school starts at 8:30 what do you do?")
-    print("1. Get up and get ready")#1
-    print("2. Get up and go on your phone")#2
-    print("3. Go back to bed for an hour")#3
+    print("1. Get up and get ready")# Line 
+    print("2. Get up and go on your phone")# line 
+    print("3. Go back to bed for an hour")# line
     choice = input(">")
     if choice == "1":
         Geting_ready()
+    elif choice == "2":
+        Go_on_Phone()
+    elif choice == "3":
+        Go_to_bed()
     else:
         print("Invalid Input Try Again")
         Waking_Up()
-def Geting_ready():
+
+def Geting_ready():#path 1
     print("You got up and ready its now 7:45 what do you do?")
-    print("1. Drive To school")#1
-    print("2. Ride with a friend to school")#2
-    print("3. Go on you phone for a little")#3
-    print("4. Fake being sick")#4
-    print("5. skip school")#5
+    print("1. Drive To school")
+    print("2. Ride with a friend to school")
+    print("3. Go on you phone for a little")
+    print("4. Fake being sick")
+    print("5. skip school")
     choice2 = input(">")
+
+def Go_on_Phone():#path 2
+    print("You just watched a funny tiktok and now you look at the time and its 8 what do you do?")
+    print("1. Go back on your phone")
+    print("2. Get ready")
+    choice3 = input(">")
+
+def Go_to_bed():
+    print("You just woke up and its 8:40 what to you do?")
+    print("1. Get ready and speed to school")
+    print("2. Just get ready slowly and go")
+    print("3. Skip school")
+    choice4 = input(">")
+
+def DriveToSchool():
+    print("You got to school at 8 what do you do know")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Waking_Up()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
